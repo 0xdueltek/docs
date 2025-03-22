5d991139bd3b9e2652562d859192e3ce1b9eba5d
idiom-bytes <idiom.bytes@gmail.com>
2025-03-21 20:11:00 -0700

    Updates

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,38 @@
+export default {
+  title: 'DU3L73K',
+  description: 'Official documentation for the Dueltek project',
+  themeConfig: {
+    logo: '/logo.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          text: 'Introduction',
+          items: [
+            { text: 'What is Dueltek?', link: '/guide/' },
+            { text: 'Hosting', link: '/guide/host' },
+            { text: 'Betting', link: '/guide/betting' }
+          ]
+        },
+        {
+          text: 'Core Concepts',
+          items: [
+            { text: 'Basic Usage', link: '/guide/basic-usage' },
+            { text: 'Advanced Features', link: '/guide/advanced-features' }
+          ]
+        }
+      ],
+    },
+    socialLinks: [
+      { icon: 'twitter', link: 'https://x.com/du3l73K/' },
+      { icon: 'telegram', link: 'https://t.me/BE_Base' }
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023-present DU3L73K'
+    }
+  }
+} 
